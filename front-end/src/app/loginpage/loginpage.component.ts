2c5accab38521cdf52ae33c3e73f7da152daf069
chakeem95 <chakeem95@gmail.com>
2021-04-07 17:57:26 -0700

    4.7 login formfield

--- front-end/src/app/loginpage/loginpage.component.ts
+++ front-end/src/app/loginpage/loginpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { user } from '../models/user'
 
 @Component({
   selector: 'app-loginpage',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginpageComponent implements OnInit {
 
+  currentUser: user = {userId: 0,
+    fname: "",
+    lname: '',
+    email: '',
+    password: '',
+    username: '',
+    photo: '',
+    posts: []
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  get emailField () {
+    return this.currentUser.email;
+  }
+
+  set emailField (str: string) {
+    this.currentUser.email = str;
+  }
+
+  get passwordField () {
+    return this.currentUser.password;
+  }
+
+  set passwordField(str: string) {
+    this.currentUser.password = str;
+  }
+
+
+  loginButton () {
+    console.log("clicked the login button");
+    console.log(this.currentUser.email + " " + this.currentUser.password)
   }
 
 }
---
+++ front-end/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    userId: number,
+    fname: string,
+    lname: string,
+    email: string,
+    password: string,
+    username: string,
+    photo: string,
+    posts: []
+}
