5499d3f35b6f901b54236ecdb4b2c67171632848
eaw10-20 <60686880+eaw10-20@users.noreply.github.com>
2021-04-08 19:12:10 -0700

    Implemented crud for server <--> s3

--- project-two/src/main/java/Tester.java
+++ project-two/src/main/java/Tester.java
@@ -4,26 +4,25 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.*;
 import org.apache.commons.io.FileUtils;
 import util.HibernateUtil;
 
 import java.io.File;
+import java.util.Map;
 
 public class Tester {
 
+    public static Map<String, String> env = System.getenv();
     public static String FILE_PATH = "src/main/resources";
     public static String BUCKET_NAME = "rev-p2-socialmedia-2102";
     public static String PHOTO_NAME = "IMG_2603.jpg";
-    //get the below from aws IAM
-    //https://youtu.be/Cnf0wXowWDM?t=428
-    public static String ACCESS_KEY_ID = "ACCESS_KEY_ID";
-    public static String ACCESS_SEC_KEY = "ACCESS_SEC_KEY";
+    public static String ACCESS_KEY_ID = System.getenv("S3_ACCESS_KEY");
+    public static String ACCESS_SEC_KEY = System.getenv("S3_SEC_KEY");
 
     public static void main(String[] args) {
+        System.out.println("The access key is: "+ ACCESS_KEY_ID);
+        System.out.println("The secret key is: "+ ACCESS_SEC_KEY);
         //create a credentials object to identify server for authentification
         AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY_ID, ACCESS_SEC_KEY);
 
@@ -34,17 +33,24 @@ public class Tester {
                 .withRegion(Regions.US_EAST_2)
                 .build();
 
-        putPhoto(s3client);
-        getPhoto(s3client);
+        //putPhoto(s3client);
+        //getPhoto(s3client);
+        //deletePhoto(s3client);
     }
 
     //to upload a photo
     public static void putPhoto(AmazonS3 s3client){
-        String filename = "pic.jpeg"; //In project every file name should be unique
-        String filepath = "A/File/Path/toupload/from";
+        String filename = "MVCdesignpattern.jpg"; //In project every file name should be unique
+        String filepath = "src/main/resources/ToUploadFromTemp/"+filename;
 
-        s3client.putObject(new PutObjectRequest(BUCKET_NAME, filename, new File(filepath))
-                .withCannedAcl(CannedAccessControlList.PublicRead));
+        try{
+            s3client.putObject(new PutObjectRequest(BUCKET_NAME, filename, new File(filepath)));
+
+            System.out.println("Successfully uploaded photo!");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
 
     }
 
@@ -57,9 +63,22 @@ public class Tester {
             S3ObjectInputStream inputStream = obj.getObjectContent();
 
             //store image at a given path for now
-            FileUtils.copyInputStreamToFile(inputStream, new File(FILE_PATH));
+            FileUtils.copyInputStreamToFile(inputStream, new File(FILE_PATH+"/"+PHOTO_NAME));
 
+            System.out.println("Successfully downloaded photo!");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
+    }
+
+    public static void deletePhoto(AmazonS3 s3client){
+        String filename = "MVCdesignpattern.jpg"; //In project every file name should be unique
+
+        try{
+            s3client.deleteObject(new DeleteObjectRequest(BUCKET_NAME, filename));
+
+            System.out.println("Successfully deleted photo");
         }catch(Exception e){
             e.printStackTrace();
         }
