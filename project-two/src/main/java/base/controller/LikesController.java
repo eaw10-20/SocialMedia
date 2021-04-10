b531ed54c0249b858ed1fb10e699411ebd7b569b
Corey Schink <corey.schink@gmail.com>
2021-04-10 11:12:56 -0400

    added all existing like methods to routes

--- project-two/src/main/java/base/controller/LikesController.java
+++ project-two/src/main/java/base/controller/LikesController.java
@@ -1,16 +1,36 @@
 package base.controller;
 
 import base.dao.LikesDaoImpl;
+import base.model.Likes;
+import base.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 @Controller
 @RequestMapping("/api")
 public class LikesController {
 
     private LikesDaoImpl likesDao;
 
+    //http://localhost:9005/social/api/likePost
+    @PostMapping(value="/likePost")
+    public void likePost(@RequestBody Likes like){
+        likesDao.addLike(like);
+    }
 
+    //http://localhost:9005/social/api/unlikePost
+    @PutMapping(value="/unlikePost")
+    public void unlikePost(@RequestBody Likes like){
+        likesDao.unLike(like);
+    }
+
+    //http://localhost:9005/social/api/getAllLikes
+    @GetMapping(value="/allLikes")
+    public Long getAllLikesOnPost(@RequestBody Likes like){
+        int id = like.getPostId();
+        return likesDao.getAllLikesOnPost(id);
+    }
 
     ////Constructors
 
