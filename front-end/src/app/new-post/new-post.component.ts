827a81159fb36dd7b7e3a7f1b1b4edb23802b369
eaw10-20 <60686880+eaw10-20@users.noreply.github.com>
2021-04-12 18:09:29 -0700

    uploading photo for post functionality work

--- front-end/src/app/new-post/new-post.component.ts
+++ front-end/src/app/new-post/new-post.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Post } from '../models/post';
 import { User } from '../models/user';
+import { Photo } from '../models/photo';
+import { PhotoServicesService } from '../services/photo-services.service';
 import { PostServicesService } from '../services/post-services.service';
 import { UserServicesService } from '../services/user-services.service';
 
@@ -19,9 +21,12 @@ export class NewPostComponent implements OnInit {
     userId: null
   }
 
-  user;
+  user: User;
+  photos: Photo[];
+  selectedFile: File;
 
-  constructor(private postService: PostServicesService, private userService: UserServicesService,private router: Router) { }
+  constructor(private postService: PostServicesService, private userService: UserServicesService, 
+    private photoService: PhotoServicesService, private router: Router) { }
 
   descriptionField: string;
 
@@ -45,10 +50,26 @@ export class NewPostComponent implements OnInit {
     )
   }
 
+  //Should store the uploaded image
+  onFileSelected(event){
+    let photo: Photo;
+    photo.imageData = event.target.files[0];
+
+    //add photo to post
+    this.post.photos.push(photo.imageData);
+
+
+  }
+
+
   sendPost() {
     this.post.userId = this.user;
     console.log(this.post)
     this.postService.createNewPost(this.post);
+    //for now making a separate call for photos. Should probably streamline this later
+    for(let pic of this.photos){
+      this.photoService.uploadPhoto(pic);
+    }
 
     this.router.navigateByUrl('/RefreshComponent', { skipLocationChange: true }).then(() => {
       this.router.navigate(['main']);
